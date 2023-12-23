9f35d6a8acebaec2d35c19de06b57771fa90f37c
Megatronhehe <satyamahendra09@gmail.com>
2023-12-23 13:34:52 +0700

    feat:frontend: redirect unauthenticated user to login page

--- frontend/src/pages/Todos.jsx
+++ frontend/src/pages/Todos.jsx
@@ -1,5 +1,14 @@
 import React, { useContext } from "react";
 
+import AuthContext from "../context/AuthContext/AuthContext";
+import { Navigate, redirect } from "react-router-dom";
+
 export default function Todos() {
+	const { user } = useContext(AuthContext);
+
+	if (!user) {
+		return <Navigate to="/login" />;
+	}
+
 	return <div>ur authenticated</div>;
 }
